542c2f64c6e64ac5ca099d1b580614e9a2c9ad40
Pfju <marcelstzb@gmx.de>
2019-06-17 23:21:26 +0200

    This is a poor commit statement.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
+# from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You are at the polls index.")
+    '''
+    The commented lines are one possibility to return a response.
+    '''
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+        }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    '''
+    Provides more details to the chosen question.
+    '''
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question}) #TODO: A shortcut: get_object_or_404() see Tutorial 3
+
+def results(request, question_id):
+    '''
+    Provides the question results.
+    '''
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    '''
+    Vote answer to question.
+    '''
+    return HttpResponse("You're voting on question %s." % question_id)
